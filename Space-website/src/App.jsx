afd56a7fb283a9275e509601fc608c6e209e2b5f
DanielMen97 <133619338+DanielMen97@users.noreply.github.com>
2024-03-06 23:45:42 -0500

    Primer commit Space

---
+++ Space-website/src/App.jsx
@@ -0,0 +1,13 @@
+import styles from './App.module.css';
+import { Navbar } from './components/navbar/navbar';
+
+function App() {
+
+  return (
+    <div className={styles.app}>
+      <Navbar></Navbar>
+    </div>
+  )
+}
+
+export default App;
---
+++ Space-website/src/components/navbar/navbar.jsx
@@ -0,0 +1,26 @@
+import styles from './navbar.module.css';
+import logo from '../../assets/shared/logo.svg'
+
+export const Navbar = () => {
+
+  return (
+    <div className={styles.navbar_container}>
+      <img src={logo}></img>
+      <div className={styles.line}>
+        hola
+      </div>
+      <nav className={styles.navbar}>
+       <ul className={styles.list_container}>
+          <li className={styles.list_item}><span>00</span>HOME</li>
+          <li className={styles.list_item}><span>01</span>DESTINATION</li>
+          <li className={styles.list_item}><span>02</span>CREW</li>
+          <li className={styles.list_item}><span>03</span>TECHNOLOGY</li>
+       </ul>
+
+      </nav>
+
+    </div>
+
+
+  )
+}
