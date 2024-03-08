1de0f962fdf5b75fd8dac36c887b64b0dec31acc
DanielMen97 <cristian.mendozapds@gmail.com>
2024-03-08 17:11:19 -0500

    Pendiente reset useState

--- Space-website/src/components/destination/destinationBody/destinationBody.jsx
+++ Space-website/src/components/destination/destinationBody/destinationBody.jsx
@@ -7,16 +7,7 @@ import { useState } from 'react'
 
 export const DestinationBody = () => {
 
-  const [ isActive, setIsActive ] = useState({
-    "name": "Moon",
-    "images": {
-      "png": "./assets/destination/image-moon.png",
-      "webp": "./assets/destination/image-moon.webp"
-    },
-    "description": "See our planet as you’ve never seen it before. A perfect relaxing trip away to help regain perspective and come back refreshed. While you’re there, take in some history by visiting the Luna 2 and Apollo 11 landing sites.",
-    "distance": "384,400 km",
-    "travel": "3 days"
-  })
+  const [ isActive, setIsActive ] = useState({})
 
   const handleIsSelect = (element) => {
     
