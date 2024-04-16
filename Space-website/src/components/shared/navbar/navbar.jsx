adf1096be435945c852425fb5c8298a33212c01f
DanielMen97 <cristian.mendozapds@gmail.com>
2024-04-16 11:06:45 -0500

    Commit Ajustes

--- Space-website/src/components/shared/navbar/navbar.jsx
+++ Space-website/src/components/shared/navbar/navbar.jsx
@@ -1,11 +1,23 @@
 import styles from './navbar.module.scss';
 import logo from '../../../assets/shared/logo.svg'
 import { NavLink, useLocation } from 'react-router-dom';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 export const Navbar = () => {
   const { pathname } = useLocation();
-  const [isOpenNavbar, setIsOpenNavbar] = useState(false)
+  const [isOpenNavbar, setIsOpenNavbar] = useState(false);
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 375);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 375);
+      if (window.innerWidth > 375) {
+        setIsOpenNavbar(false);
+      }};
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };}, []);
 
   return (
     <div className={styles.navbar_container}>
@@ -18,26 +30,29 @@ export const Navbar = () => {
         <img className={styles.iconClose} onClick={() => setIsOpenNavbar(false)} src='./src/assets/shared/icon-close.svg'>
         </img>
         <ul className={styles.list_container}>
-          <NavLink
-            to='/'
+          <NavLink to='/'
+            onClick={() => setIsOpenNavbar(false)}
             className={pathname === "/" ? styles.LinkIsActive : styles.LinkIsDisable}>
             <li className={styles.list_item}>
               <span><strong>00</strong> HOME </span>
             </li>
           </NavLink>
           <NavLink to='/destination'
+            onClick={() => setIsOpenNavbar(false)}
             className={pathname === "/destination" ? styles.LinkIsActive : styles.LinkIsDisable}>
             <li className={styles.list_item}>
               <span><strong>01</strong> DESTINATION </span>
             </li>
           </NavLink>
           <NavLink to='/crew'
+            onClick={() => setIsOpenNavbar(false)}
             className={pathname === "/crew" ? styles.LinkIsActive : styles.LinkIsDisable}>
             <li className={styles.list_item}>
               <span><strong>02</strong> CREW </span>
               </li>
           </NavLink>
           <NavLink to='/technology'
+            onClick={() => setIsOpenNavbar(false)}
             className={pathname === "/technology" ? styles.LinkIsActive : styles.LinkIsDisable}>
             <li className={styles.list_item}>
               <span><strong>03</strong> TECHNOLOGY </span></li>
